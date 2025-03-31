8537b0ffcb378a2e5e5dd7ea1b11971f9f4e201f
JosephWondimagnehu <joseph.wondimagnehu@stu.ocsb.ca.>
2025-03-31 12:41:56 -0400

    Added tax.py and constants.py

---
+++ constants.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+# Created By: Joseph Wondimagnehu
+# Date: Mar. 31, 2025
+# This module contains constants.
+
+HST_RATE = 0.15
--- tax.py
+++ tax.py
@@ -1,13 +1,31 @@
 #!/usr/bin/env python3
 # Created By: Joseph Wondimagnehu
-# Date: Month Day, Year
+# Date: Mar. 31, 2025
 """
-Add something.
+This program asks the user for a subtotal.
+It then calculates and displays the tax and total
+based on the Nova Scotia HST (15%).
 """
+
+# Import the constants module to utilize the tax rate.
+import constants
+
 
 # Define the main function.
 def main():
-    pass
+    # Get the subtotal from the user.
+    subtotal = float(input("Enter the desired subtotal: $"))
+
+    # Calculate the tax, multiplying it with the Nova Scotia HST rate (15%).
+    tax = subtotal * constants.HST_RATE
+
+    # Calculate the total.
+    total = subtotal + tax
+
+    # Display the tax to the user.
+    print(f"The tax is ${tax:.2f}.")
+    # Display the total to the user.
+    print(f"The total cost is ${total:.2f}.")
 
 
 # Check if the special name of the file is __main__.
